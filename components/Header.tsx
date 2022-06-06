2d99976fb927f8ee86c44d95919bf18902f2f741
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-06 21:13:36 +0530

    netflix clone header

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10" >
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain" 
+        />
+        <ul className="hidden">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+
+        </ul>
+      </div>
+      <div>
+        
+      </div> 
+    </header>
+  )
+}
+
+export default Header
